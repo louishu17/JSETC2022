6810180275281260b338329dced47ad56ea7237b
jeremyzhou <jeremy.q.zhou@gmail.com>
2022-07-16 11:09:18 -0700

    Replace team name with one word

--- sample-bot.py
+++ sample-bot.py
@@ -13,7 +13,7 @@ import json
 
 # ~~~~~============== CONFIGURATION  ==============~~~~~
 # Replace "REPLACEME" with your team name!
-team_name = "Shiner Perch"
+team_name = "shinerperch"
 
 # ~~~~~============== MAIN LOOP ==============~~~~~
 
