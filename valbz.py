6f44d47922ae7e49b5ba65500efb0b66d69d0ee0
pyuan <py95@cornell.edu>
2022-07-16 13:58:09 -0700

    Update valbz.py

--- valbz.py
+++ valbz.py
@@ -12,6 +12,8 @@ net = 0
 def valbz_order(message, history):
     orders = []
     if message["symbol"] == "VALE":
+        if len(message["buy"]) == 0 or len(message["sell"]) == 0:
+            return []
         vale_bid_price = message["buy"][0]  # these are tuples
         vale_ask_price = message["sell"][0]
         valbz = history.last_ba("VALBZ")
@@ -20,14 +22,14 @@ def valbz_order(message, history):
             fair_price = (valbz_bid_price[0] + valbz_ask_price[0]) / 2
             if fair_price > 0.01:
                 print("got here!")
-                if vale_bid_price[0] / fair_price > 1.01:
+                if vale_bid_price[0] / fair_price > 1.00:
                     # sell VALE
                     price = vale_bid_price[0] - 0.01
                     orders.append(
                         dict(order_id=get_order_id(), symbol="VALE", dir=Dir.SELL, price=price, size=vale_bid_price[1])
                     )
                     # sells.append(orders[-1])
-                elif vale_ask_price[0] / fair_price < 0.99:
+                elif vale_ask_price[0] / fair_price < 1.00:
                     # buy VALE
                     price = vale_bid_price[0] + 0.01
                     orders.append(
