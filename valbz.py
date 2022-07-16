fe7221a484c2e4c59196f9136cdb25965423d9ce
pyuan <py95@cornell.edu>
2022-07-16 14:26:19 -0700

    Update valbz.py

--- valbz.py
+++ valbz.py
@@ -2,6 +2,7 @@ from enum import Enum
 import random
 
 CANCEL_IN = 15
+CLOSE_IN = 15
 
 class Dir(str, Enum):
     BUY = "BUY"
@@ -11,14 +12,21 @@ def get_order_id():
     return random.randint(1, 4294967290)
 
 order_ids = {}
+close_future_orders = {}
 net = 0
+
+def invert_order(order):
+    order = order.copy()
+    order["dir"] = Dir.BUY if order["dir"] == Dir.SELL else Dir.SELL
+    return order
 
 def valbz_order(message, history, tick):
     orders = []
     cancels = []
     if message["symbol"] == "VALE":
+        close_future_orders[tick] = []
         if len(message["buy"]) == 0 or len(message["sell"]) == 0:
-            return []
+            return orders, cancels
         vale_bid_price = message["buy"][0]  # these are tuples
         vale_ask_price = message["sell"][0]
         valbz = history.last_ba("VALBZ")
@@ -27,21 +35,22 @@ def valbz_order(message, history, tick):
             fair_price = (valbz_bid_price[0] + valbz_ask_price[0]) / 2
             if fair_price > 0.01:
                 print("got here!")
-                if vale_bid_price[0] / fair_price > 1.00:
+                if vale_bid_price[0] / fair_price > 1.01:
                     # sell VALE
                     price = vale_bid_price[0]
                     orders.append(
                         dict(order_id=get_order_id(), symbol="VALE", dir=Dir.SELL, price=price, size=vale_bid_price[1])
                     )
-                    # sells.append(orders[-1])
-                elif vale_ask_price[0] / fair_price < 1.00:
+
+                elif vale_ask_price[0] / fair_price < 0.99:
                     # buy VALE
                     price = vale_ask_price[0]
                     orders.append(
                         dict(order_id=get_order_id(), symbol="VALE", dir=Dir.BUY, price=price, size=vale_ask_price[1])
                     )
-                    # buys.append(orders[-1])
-    order_ids[tick + CANCEL_IN] = []
+    # add our orders to be closed in the future
+    close_future_orders[tick + CLOSE_IN] = [invert(o) for o in orders]
+    orders += close_future_orders[tick] if tick in close_future_orders else []
     for order in orders:
         order_ids[tick + CANCEL_IN].append(order["order_id"])
     if tick in order_ids:
