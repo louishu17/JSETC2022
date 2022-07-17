e6962a169c7f64ed3ffddc4daac12734ba455bb5
pyuan <py95@cornell.edu>
2022-07-16 17:11:58 -0700

    Update sample-bot-pat.py

--- sample-bot-pat.py
+++ sample-bot-pat.py
@@ -25,17 +25,14 @@ class BondStrategy:
     def bondStrategy(buys, sells):
         buy_orders = []
         sell_orders = []
-        for i in range(len(sells)):
-            if sells[i][0] < 1000:
-                buy_orders.append(
-                    dict(order_id=get_order_id(), symbol="BOND",
-                         dir=Dir.BUY, price=sells[i][0], size=sells[i][1])
-                )
-
-        for i in range(len(buys)):
-            if buys[i][0] > 1000:
-                sell_orders.append(
-                    dict(order_id=get_order_id(), symbol="BOND", dir=Dir.SELL, price=buys[i][0], size=buys[i][1]))
+        buy_orders.append(
+            dict(order_id=get_order_id(), symbol="BOND",
+                 dir=Dir.BUY, price=999, size=100)
+        )
+        sell_orders.append(
+            dict(order_id=get_order_id(), symbol="BOND",
+                 dir=Dir.SELL, price=1001, size=100)
+        )
         return buy_orders, sell_orders
 
 
